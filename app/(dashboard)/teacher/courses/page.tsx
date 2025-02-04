13d6e6ad9ec61152de46aaab94a92ac64dc1e1bf
mdeport <mateo.deport@ecole-isitech.fr>
2025-02-04 15:16:35 +0100

    add first page with search bar

---
+++ app/(dashboard)/teacher/courses/page.tsx
@@ -0,0 +1,22 @@
+import Search from "@/app/ui/search";
+import { CreateCourses } from "@/app/ui/teacher/buttons";
+
+export default function Page() {
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`text-2xl`}>Courses</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search Courses..." />
+        <CreateCourses />
+      </div>
+      {/* <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense> */}
+      <div className="mt-5 flex w-full justify-center">
+        {/* <Pagination totalPages={totalPages} /> */}
+      </div>
+    </div>
+  );
+}
