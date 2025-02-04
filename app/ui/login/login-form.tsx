8066acbca0ec57f6fb93215500025f9daa599f6f
AlexisCro <95683483+AlexisCro@users.noreply.github.com>
2025-02-04 16:27:25 +0100

    login action

--- app/ui/login/login-form.tsx
+++ app/ui/login/login-form.tsx
@@ -16,10 +16,10 @@ export default function LoginForm() {
     <div className='w-full max-w-xs'>
       <form action={formAction} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
         <div className="mb-4">
-          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-            Username
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+            Email
           </label>
-          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username" />
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" placeholder="Email" />
         </div>
 
         <div className="mb-6">
@@ -32,7 +32,7 @@ export default function LoginForm() {
 
         <div className="flex items-center justify-between">
           <input type="hidden" name="redirectTo" value={callbackUrl} />
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" aria-disabled={isPending} type="button">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" aria-disabled={isPending}>
             Sign In
           </button>
           <a className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800" href="#">
