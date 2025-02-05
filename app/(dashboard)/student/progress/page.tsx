4eab092589a0142ce26bde9b3514220093ce3ab3
clemberard <clement.berard@ecole-isitech.fr>
2025-02-05 12:10:00 +0100

    add skeleton table progresses

--- app/(dashboard)/student/progress/page.tsx
+++ app/(dashboard)/student/progress/page.tsx
@@ -1,13 +1,15 @@
 import {ProgressesTable} from "@/app/ui/student/table";
 import {Suspense} from 'react';
-import {CoursesTableSkeleton} from '@/app/ui/skeletons';
+import {ProgressesTableSkeleton} from '@/app/ui/skeletons';
 
 export default function Page() {
     return (
         <>
             <h1 className="text-4xl font-bold text-center mt-4">Progresses</h1>
             <div className="container mx-auto mt-6">
-                <ProgressesTable/>
+                <Suspense fallback={<ProgressesTableSkeleton />}>
+                    <ProgressesTable/>
+                </Suspense>
             </div>
         </>
     );
