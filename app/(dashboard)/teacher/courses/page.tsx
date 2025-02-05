9fbeeebd8c5395b28d99a0edd5d0a863c747d92a
AlexisCro <95683483+AlexisCro@users.noreply.github.com>
2025-02-05 13:41:42 +0100

    navbar dashboard teacher

--- app/(dashboard)/teacher/courses/page.tsx
+++ app/(dashboard)/teacher/courses/page.tsx
@@ -4,6 +4,7 @@ import { fetchCoursesPages } from "@/app/lib/data";
 import { CoursesTableSkeleton } from "@/app/ui/skeleton";
 import { Suspense } from "react";
 import { CoursesTableFiltre } from "@/app/ui/teacher/table";
+import Navbar from "@/app/ui/layout/navbar";
 
 export default async function Page(props: {
   searchParams?: Promise<{
@@ -16,19 +17,23 @@ export default async function Page(props: {
   const currentPage = Number(searchParams?.page) || 1;
   //const totalPages = await fetchCoursesPages(query);
   return (
-    <div className="w-full">
-      <div className="flex w-full items-center justify-between">
-        <h1 className={`text-2xl`}>Courses</h1>
+    <>
+      <Navbar />
+
+      <div className="w-full">
+        <div className="flex w-full items-center justify-between">
+          <h1 className={`text-2xl`}>Courses</h1>
+        </div>
+        <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+          <Search placeholder="Search Courses..." />
+          <CreateCourses />
+        </div>
+        <div>
+          <Suspense key={query + currentPage} fallback={<CoursesTableSkeleton />}>
+            <CoursesTableFiltre query={query} currentPage={currentPage} />
+          </Suspense>
+        </div>
       </div>
-      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-        <Search placeholder="Search Courses..." />
-        <CreateCourses />
-      </div>
-      <div>
-        <Suspense key={query + currentPage} fallback={<CoursesTableSkeleton />}>
-          <CoursesTableFiltre query={query} currentPage={currentPage} />
-        </Suspense>
-      </div>
-    </div>
+    </>
   );
 }
--- app/ui/layout/navbar.tsx
+++ app/ui/layout/navbar.tsx
@@ -32,8 +32,8 @@ export default async function Navbar() {
                           className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Services</Link>
                   </li>
                   <li>
-                      <Link href="#"
-                          className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Pricing</Link>
+                      <Link href="/teacher/courses" passHref
+                          className="block py-2 px-3 text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent">Courses</Link>
                   </li>
                   <li>
                       <Link href="/register" passHref
