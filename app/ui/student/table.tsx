ee616832110927b7d70dd29ccd36184ab6efc559
clemberard <clement.berard@ecole-isitech.fr>
2025-02-06 10:24:30 +0100

    fix btn table

--- app/ui/student/table.tsx
+++ app/ui/student/table.tsx
@@ -33,22 +33,22 @@ export async function CoursesTable() {
                                 className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
                             >
                                 <td className="whitespace-nowrap py-3 pl-6 pr-3">
-                                    {course.enrollment ? (
-                                        course.enrollment.status
-                                    ) : (
-                                        <Link target={'_blank'} href={`/student/courses/${course.id}/show`} className={"font-medium text-blue-600 dark:text-blue-500 hover:underline"}>
-                                            {course.title}
-                                        </Link>
-                                    )}
+                                    <Link target={'_blank'} href={`/student/courses/${course.id}/show`} className={"font-medium text-blue-600 dark:text-blue-500 hover:underline"}>
+                                        {course.title}
+                                    </Link>
                                 </td>
                                 <td className="whitespace-nowrap px-3 py-3">{course.instrument}</td>
                                 <td className="whitespace-nowrap px-3 py-3">{course.level}</td>
                                 <td className="whitespace-nowrap px-3 py-3">{course.schedule}</td>
                                 <td className="whitespace-nowrap px-3 py-3">{course.capacity}</td>
                                 <td className="whitespace-nowrap px-3 py-3">
+                                    {course.enrollment ? (
+                                        course.enrollment.status
+                                    ) : (
                                     <EnrollmentButton userId={user.id} courseId={course.id}>
                                         Enroll
                                     </EnrollmentButton>
+                                    )}
                                 </td>
                             </tr>
                         ))}
